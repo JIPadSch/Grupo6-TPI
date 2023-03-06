a335a0190ce62804951de8dde39cb32f33ff9615
Juan Ignacio Padrón Schneider <jignacio.padron@est.fi.uncoma.edu.ar>
2023-03-06 16:30:31 -0300

    Primer commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+/**
+ * @author 
+ * Nicolas Guidara
+ * Juan Ignacio Padron Schneider
+ * Victoria de Goycochea
+ * Javier Peron
+ * Gabriel Alberto Ferreyra
+ * Ciro Nicolas Molina
+ *
+ */
+public static void main(String[] args) {
+    
+}
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author padro
- *
- */
-module TPI {
-}
